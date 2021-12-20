b769e0ce55bfd479f04c5f211f34828ea2e651b5
unknown <saikumarar104@gmail.com>
2021-12-20 21:30:10 +0530

    changes made

--- src/Routes/purchasedcourses_route.js
+++ src/Routes/purchasedcourses_route.js
@@ -1,6 +1,6 @@
 var express=require('express');
 const router= express.Router();
-const views=require('../controllers/purchasedcourses')
+const views=require('../controllers/purchasedCourses')
 const verify=require('../Middleware/middleware')
 
 router.post('/addpurchasedcourses',verify.authenticateToken,views.addpurchasedcourses)
